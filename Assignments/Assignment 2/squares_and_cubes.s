ff63e045ce5d04774339085bb719117e0f128ba0
D-A-V-3 <144196292+D-A-V-3@users.noreply.github.com>
2023-11-10 17:18:20 -0800

    Update squares_and_cubes.s

--- Assignments/Assignment 2/squares_and_cubes.s
+++ Assignments/Assignment 2/squares_and_cubes.s
@@ -33,38 +33,44 @@ input: 	.word 0
 	//Max
 	mov r6, #25
 
+	//Square accumulator
 	mov r7, #0
+	//Cube accumulator
 	mov r8, #0
+
 	//Input Validation
 	start://do
 	ldr r0, =prompt
 	bl printf
 
+	//Gather input
 	ldr r0, =scnstr
 	ldr r1, =input
 	bl scanf
 
 	ldr r1, =input
-	ldr r5, [r1]
+	ldr r5, [r1]  //r5 = user input
 
-	if://if r5 < r4
+	if://if r5(input) < r4(min)
 	cmp r5, r4
 	bge else
 	ldr r0, =errorm
 	bl printf
 	b endif
-	else://else if r5 > r6
+	else://else if r5(input) > r6(max)
 	cmp r5, r6
 	ble endif
 	ldr r0, =errorm
 	bl printf
 	endif:
 
-	//while r5 < r4 or r5 > r6
+	//while r5(input) < r4(min) or r5(input) > r6(max)
 	cmp r5, r4
 	blt start
+
 	cmp r5, r6
 	bgt start
+	//End of do-while
 
 	//Print headers
 	ldr r0, =newline
@@ -80,24 +86,29 @@ input: 	.word 0
 	bl printf
 
 	//Display numbers and there squares and cubes
-	while:
+	while: //While r4(min) <= r5(input)
 	cmp r4, r5
 	bgt endWhile
+
 	ldr r0, =output1
 
-	mov r1, r4
-	mul r2, r4, r4
-	mul r3, r2, r4
+	mov r1, r4     //Original number
+	mul r2, r4, r4 //Number squared
+	mul r3, r2, r4 //Number cubed
 
+	//Update accumulators
 	add r7, r7, r2
 	add r8, r8, r3
 
 	bl printf
+
+	//Update loop count
 	add r4, #1
 	b while
+
 	endWhile:
 
-	//Outputs of sums and cubes
+	//Outputs sums of squares and cubes
 	ldr r0, =dashes
 	bl printf
 
