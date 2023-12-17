2bcb1515a1cb5e45fcb6e92b5f69384f2ac60f05
D-A-V-3 <144196292+D-A-V-3@users.noreply.github.com>
2023-12-16 23:15:35 -0800

    Update Final.s

--- Projects/Final/Final.s
+++ Projects/Final/Final.s
@@ -6,13 +6,21 @@
 .equ GREEN_PIN, 2
 .equ BLUE_PIN, 3
 .equ RED_PIN, 25
+.equ BUTTON, 23
 
 .align 4
+.section .rodata
+out: .asciz "%d\n"
 .text
 .global main
 main:
 	push {lr}
 	bl wiringPiSetup
+
+	mov r4, #0
+	mov r0, #BUTTON
+	mov r1, #INPUT
+	bl pinMode
 
 	mov r0, #YEL_PIN
 	mov r1, #OUTPUT
@@ -30,9 +38,10 @@ main:
 	mov r1, #OUTPUT
 	bl pinMode
 
-	mov r4, #0
+//	mov r4, #0
 	loop:
 	//Yellow
+	yellowloop:
 	mov r0,#YEL_PIN
 	mov r1, #HIGH
 	bl digitalWrite
@@ -45,6 +54,9 @@ main:
 	bl digitalWrite
 
 	//Green
+	cmp r4, #3
+	beq endLoop
+	greenloop:
 	mov r0,#GREEN_PIN
         mov r1, #HIGH
         bl digitalWrite
@@ -57,6 +69,9 @@ main:
         bl digitalWrite
 
 	//Blue
+	cmp r4, #2
+	beq endLoop
+	blueloop:
         mov r0,#BLUE_PIN
         mov r1, #HIGH
         bl digitalWrite
@@ -69,6 +84,9 @@ main:
         bl digitalWrite
 
 	//Red
+	cmp r4, #1
+	beq endLoop
+	redloop:
         mov r0,#RED_PIN
         mov r1, #HIGH
         bl digitalWrite
@@ -80,10 +98,96 @@ main:
         mov r1, #LOW
         bl digitalWrite
 
+//	add r4, #1
+//	cmp r4,#1
+//	blt loop
+	endLoop:
+
+	butloop:
+	mov r0, #BUTTON
+	bl digitalRead
+	cmp r0, #HIGH
+	bne loop
+
+//	beq end
+
+
+//	mov r0, #RED_PIN
+//	mov r1, #HIGH
+//	bl digitalWrite
+
+    //    mov r0, #RED_PIN
+  //      mov r1, #LOW
+//        bl digitalWrite
+
+	bl end
+
+	end:
 	add r4, #1
-	cmp r4,#2
-	blt loop
+	cmp r4, #1
+	bne blue
+	mov r0, #RED_PIN
+	mov r1, #HIGH
+	bl digitalWrite
+	mov r0, r4
+	ldr r0, =out
+	mov r1, r4
+	bl printf
+	bl loop
+	blue:
+	cmp r4, #2
+	bne green
+	mov r0, #BLUE_PIN
+	mov r1, #HIGH
+	bl digitalWrite
+        mov r0, r4
+        ldr r0, =out
+        mov r1, r4
+        bl printf
+
+	bl loop
+	green:
+	cmp r4, #3
+	bne endProg
+	mov r0, #GREEN_PIN
+	mov r1, #HIGH
+	bl digitalWrite
+        mov r0, r4
+        ldr r0, =out
+        mov r1, r4
+        bl printf
+
+	bl loop
+	yellow:
+	cmp r4, #4
+	bne endProg
+	mov r0, #YEL_PIN
+	mov r1, #HIGH
+	bl digitalWrite
+        mov r0, r4
+        ldr r0, =out
+        mov r1, r4
+        bl printf
+
+
+	endProg:
+
+	mov r0, #RED_PIN
+        mov r1, #LOW
+        bl digitalWrite
+
+        mov r0, #BLUE_PIN
+        mov r1, #LOW
+        bl digitalWrite
+
+        mov r0, #GREEN_PIN
+        mov r1, #LOW
+        bl digitalWrite
+
+        mov r0, #YEL_PIN
+        mov r1, #LOW
+        bl digitalWrite
+
 
 	mov r0, #0
 	pop {pc}
-
